aa853ba7818e2b0bd56f8f5b8fa1e4e720afea69
svadl7 <105679798+svadl7@users.noreply.github.com>
2023-12-02 14:10:00 -0500

    connection_chips.js

--- verify/connection_chips.js
+++ verify/connection_chips.js
@@ -1,6 +1,6 @@
 module.exports = {
     connectionurl: "mongodb+srv://vadlamudisai1:Jaswanth3579@webtrail.o1tsui4.mongodb.net/?retryWrites=true&w=majority",
     database: 'Grocery_store',
-    collection: 'Groceries'
+    collection: 'Chips'
   };
   
