f2d3d69f822b786fd05bd88a81f7d9876f0af7bb
svadl7 <svadl7@unh.newhaven.edu>
2023-12-02 13:49:22 -0500

    first commit

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const http = require("http");
 const path = require("path");
 const fs = require("fs");
-const mongo_client= require('mongodb').MongoClient;
-const mongoconnection_details = require('./verify/connection.js');
-const mongo_client_connection = new mongo_client(mongoconnection_details.connectionurl);
+const client= require('mongodb').MongoClient;
+const url=require("url");
+const mongoconnection_details_chips = require('./verify/connection_chips.js');
+const mongo_client_connection_chips = new client(mongoconnection_details_chips.connectionurl);
+
+const mongoconnection_details_drinks = require('./verify/connection_drinks.js');
+const mongo_client_connection_drinks = new client(mongoconnection_details_drinks.connectionurl);
+
+
+const mongoconnection_details_tobacco = require('./verify/connection_tobacco.js');
+const mongo_client_connection_tobacco = new client(mongoconnection_details_tobacco.connectionurl);
+
+
+const mongoconnection_details_milk = require('./verify/connection_milk.js');
+const mongo_client_connection_milk = new client(mongoconnection_details_milk.connectionurl);
+
 
 const server = http.createServer((req, res) => {
+    const parsedUrl = url.parse(req.url, true);
+    const sub_path = parsedUrl.pathname;
     if (req.url === '/') {
-        // read public.html file from public folder
         fs.readFile(path.join(__dirname, 'public', 'index.html'),
                     (err, content) => {
                                     
@@ -25,70 +39,121 @@ const server = http.createServer((req, res) => {
             "Access-Control-Allow-Methods": "OPTIONS, POST, GET",
             "Content-Type": 'application/json'
         };
-        (async(req,res)=>{
-            try
-            {
-                await mongo_client_connection.connect();
-                const database = mongo_client_connection.db(mongoconnection_details.database);
-                const collection = database.collection(mongoconnection_details.collection);
-                if (req.method === 'GET') {
-                    var docs_json = await collection.find({}).toArray();
-                    docs_json = JSON.stringify(docs_json, null, 2);
-                    fs.writeFile('./public/db.json', docs_json, () => {});
-                    res.writeHead(200, headers);
-                    res.end(docs_json);
-                    console.log(docs_json);
-                  }
-                  else {
-                    res.writeHead(405, {'Content-Type': 'text/plain'});
-                    res.end('Method Not supported');
-                  } 
-            }catch (err) {
-                console.error(err);
-                res.writeHead(500, {'Content-Type': 'text/plain'});
-                res.end('server failed to handle the request');
-              }
-        })(req,res);
+        switch (sub_path){
+            case '/api/chips':
+                (async(req,res)=>{
+                    try
+                    {
+                        await mongo_client_connection_chips.connect();
+                        const database = mongo_client_connection_chips.db(mongoconnection_details_chips.database);
+                        const collection = database.collection(mongoconnection_details_chips.collection);
+                        if (req.method === 'GET') {
+                            var docs_json = await collection.find({}).toArray();
+                            docs_json = JSON.stringify(docs_json, null, 2);
+                            res.writeHead(200, headers);
+                            res.end(docs_json);
+                            console.log(docs_json);
+                          }
+                          else {
+                            res.writeHead(405, {'Content-Type': 'text/plain'});
+                            res.end('Method Not supported');
+                          } 
+                    }catch (err) {
+                        console.error(err);
+                        res.writeHead(500, {'Content-Type': 'text/plain'});
+                        res.end('server failed to handle the request');
+                      }
+                })(req,res);
+                break;
+            case '/api/drinks':
+                (async(req,res)=>{
+                    try
+                    {
+                        await mongo_client_connection_drinks.connect();
+                        const database = mongo_client_connection_drinks.db(mongoconnection_details_drinks.database);
+                        const collection = database.collection(mongoconnection_details_drinks.collection);
+                        if (req.method === 'GET') {
+                            var docs_json = await collection.find({}).toArray();
+                            docs_json = JSON.stringify(docs_json, null, 2);
+                            res.writeHead(200, headers);
+                            res.end(docs_json);
+                            console.log(docs_json);
+                          }
+                          else {
+                            res.writeHead(405, {'Content-Type': 'text/plain'});
+                            res.end('Method Not supported');
+                          } 
+                    }catch (err) {
+                        console.error(err);
+                        res.writeHead(500, {'Content-Type': 'text/plain'});
+                        res.end('server failed to handle the request');
+                      }
+                })(req,res);
+                break;
+            case '/api/tobacco':
+                (async(req,res)=>{
+                    try
+                    {
+                        await mongo_client_connection_tobacco.connect();
+                        const database = mongo_client_connection_tobacco.db(mongoconnection_details_tobacco.database);
+                        const collection = database.collection(mongoconnection_details_tobacco.collection);
+                        if (req.method === 'GET') {
+                            var docs_json = await collection.find({}).toArray();
+                            docs_json = JSON.stringify(docs_json, null, 2);
+                            res.writeHead(200, headers);
+                            res.end(docs_json);
+                            console.log(docs_json);
+                          }
+                          else {
+                            res.writeHead(405, {'Content-Type': 'text/plain'});
+                            res.end('Method Not supported');
+                          } 
+                    }catch (err) {
+                        console.error(err);
+                        res.writeHead(500, {'Content-Type': 'text/plain'});
+                        res.end('server failed to handle the request');
+                      }
+                })(req,res);
+                break;
+            case '/api/milk':
+                (async(req,res)=>{
+                    try
+                    {
+                        await mongo_client_connection_milk.connect();
+                        const database = mongo_client_connection_milk.db(mongoconnection_details_milk.database);
+                        const collection = database.collection(mongoconnection_details_milk.collection);
+                        if (req.method === 'GET') {
+                            var docs_json = await collection.find({}).toArray();
+                            docs_json = JSON.stringify(docs_json, null, 2);
+                            res.writeHead(200, headers);
+                            res.end(docs_json);
+                            console.log(docs_json);
+                          }
+                          else {
+                            res.writeHead(405, {'Content-Type': 'text/plain'});
+                            res.end('Method Not supported');
+                          } 
+                    }catch (err) {
+                        console.error(err);
+                        res.writeHead(500, {'Content-Type': 'text/plain'});
+                        res.end('server failed to handle the request');
+                      }
+                })(req,res);
+                break;
+            default:
+                res.writeHead(404, { 'Content-Type': 'text/plain' });
+                res.end('Page Not Found');
+
+ 
 
 
-        // read the about.html file public folder
-        // fs.readFile(
-        //     path.join(__dirname, 'public', 'about.html'),
-        //             (err, content) => {
-                                    
-        //                             if (err) throw err;
-        //                             res.writeHead(200, { 'Content-Type': 'text/html' });
-        //                             res.end(content);
-        //                 }
-        //       );
+
+        }
+
      }
-    else if (req.url==='/api')
-    {
-        fs.readFile(
-            path.join(__dirname, 'public', 'db.json'),'utf-8',
-                    (err, content) => {
-                                    
-                                    if (err) throw err;
-                                    // Please note the content-type here is application/json
-                                    res.writeHead(200, { 'Content-Type': 'application/json' });
-                                    res.end(content);
-                        }
-              );
-    }
     else{
         res.end("<h1> 404 nothing is here</h1>");
     }
-
-    /*
-
-        But what if we have  1000 pages/urls ? do we need to write 1000 if-else statements?
-
-    /*/
 });
-
-// it will first try to look for
-// environment variable, if not found then go for 5959
 const PORT= 2306;
-
-// port, callback
 server.listen(PORT,()=> console.log(`Great our server is running on port ${PORT} `));
